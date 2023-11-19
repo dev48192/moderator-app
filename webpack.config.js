e4f7da43e1aab179180de71cc0ce5ecdd6354c7c
Buddhadeb Das <dev48192@gmail.com>
2023-11-19 10:14:31 +0530

    commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,55 @@
 const { resolve } = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const DotenvWebpackPlugin = require("dotenv-webpack");
 
-module.exports = {
-  mode: "development",
-  entry: ["regenerator-runtime/runtime.js", "./src/index.tsx"],
-  output: {
-    path: resolve(__dirname, "build"),
-    publicPath: "auto",
-  },
-  resolve: {
-    extensions: [".js", ".jsx", ".ts", ".tsx"],
-  },
-  devServer: {
-    historyApiFallback: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.[jt]sx?$/,
-        use: "babel-loader",
-        exclude: /node_modules/,
+module.exports = async (_, { stats, mode = 'development' }) => {
+  const { APP_ENV = 'development' } = process.env;
+  const isDev = mode === 'development';
+  return {
+    mode,
+    entry: ["regenerator-runtime/runtime.js", "./src/index.tsx"],
+    output: {
+      path: resolve(__dirname, "build"),
+      publicPath: "auto",
+    },
+    resolve: {
+      extensions: [".js", ".jsx", ".ts", ".tsx"],
+    },
+    ...(isDev && {
+      devServer: {
+        port: 3002,
+        historyApiFallback: true,
+        hot: true,
+        open: true,
       },
-      {
-        test: /\.module\.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
-      },
-      {
-        test: /\.(png|jpe?g|svg)$/,
-        type: "asset/resource",
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/index.html",
-      filename: "index.html",
-      inject: "body",
     }),
-  ],
+    module: {
+      rules: [
+        {
+          test: /\.[jt]sx?$/,
+          use: "babel-loader",
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.module\.scss$/,
+          use: ["style-loader", "css-loader", "sass-loader"],
+        },
+        {
+          test: /\.(png|jpe?g|svg)$/,
+          type: "asset/resource",
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./src/index.html",
+        filename: "index.html",
+        inject: "body",
+      }),
+
+      new DotenvWebpackPlugin({
+        path: resolve(__dirname, 'config', APP_ENV, '.defined.urls'),
+      })
+    ],
+  }
 };
